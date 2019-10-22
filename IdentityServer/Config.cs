33d7200ac5b9bc01f4f8a76f3e9e6841a9ff7fcd
BaWu <wullemsb@gmail.com>
2019-10-22 10:18:43 +0200

    Users

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -2,8 +2,11 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
+using IdentityServer4.Test;
 using System.Collections.Generic;
+using System.Security.Claims;
 
 namespace IdentityServer
 {
@@ -11,17 +14,61 @@ namespace IdentityServer
     {
         public static IEnumerable<IdentityResource> Ids =>
             new IdentityResource[]
-            { 
+            {
                 new IdentityResources.OpenId()
             };
 
         public static IEnumerable<ApiResource> Apis =>
-            new ApiResource[] 
+            new ApiResource[]
             { };
-        
+
         public static IEnumerable<Client> Clients =>
-            new Client[] 
-            { };
-        
+            new Client[]
+            {
+                new Client {
+                    ClientId = "mvc",
+                    ClientName = "MVC Client",
+                    Enabled = true,
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    RequireConsent = true,
+                    AllowRememberConsent = true,
+                    RedirectUris =
+                      new List<string> {"https://localhost:44356/signin-oidc"},
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
+                }
+            };
+
+        public static List<TestUser> Users =>
+             new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "alice",
+                    Password = "password",
+
+                    Claims = new []
+                    {
+                        new Claim("name", "Alice"),
+                        new Claim("website", "https://alice.com")
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "bob",
+                    Password = "password",
+
+                    Claims = new []
+                    {
+                        new Claim("name", "Bob"),
+                        new Claim("website", "https://bob.com")
+                    }
+                }
+            };
     }
 }
