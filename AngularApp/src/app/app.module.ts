111071a959c516ac79cefbc9307afed996b3373e
BaWu <wullemsb@gmail.com>
2019-10-23 09:34:16 +0200

    ImplicitFlow

---
+++ AngularApp/src/app/app.module.ts
@@ -0,0 +1,60 @@
+import { HttpClientModule } from '@angular/common/http';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AuthModule, ConfigResult, OidcConfigService, OidcSecurityService, OpenIdConfiguration } from 'angular-auth-oidc-client';
+import { AppComponent } from './app.component';
+
+const oidc_configuration = 'assets/auth.clientConfiguration.json';
+// if your config is on server side
+// const oidc_configuration = ${window.location.origin}/api/ClientAppSettings
+
+export function loadConfig(oidcConfigService: OidcConfigService) {
+    return () => oidcConfigService.load(oidc_configuration);
+}
+
+@NgModule({
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        RouterModule.forRoot([
+            { path: '', component: AppComponent },
+            { path: 'home', component: AppComponent },
+            { path: 'forbidden', component: AppComponent },
+            { path: 'unauthorized', component: AppComponent },
+        ]),
+        AuthModule.forRoot(),
+    ],
+    providers: [
+        OidcConfigService,
+        {
+            provide: APP_INITIALIZER,
+            useFactory: loadConfig,
+            deps: [OidcConfigService],
+            multi: true,
+        },
+    ],
+    bootstrap: [AppComponent],
+})
+export class AppModule {
+    constructor(private oidcSecurityService: OidcSecurityService, private oidcConfigService: OidcConfigService) {
+        this.oidcConfigService.onConfigurationLoaded.subscribe((configResult: ConfigResult) => {
+		
+            // Use the configResult to set the configurations		
+            const config: OpenIdConfiguration = {
+                stsServer: configResult.customConfig.stsServer,
+                redirect_url: 'http://localhost:4200',
+                client_id: 'angularClient',
+                scope: 'openid profile',
+                response_type: 'id_token token',
+                silent_renew: false,
+                silent_renew_url: 'http://localhost:4200/silent-renew.html',
+                log_console_debug_active: true,
+                // all other properties you want to set
+            };
+
+            this.oidcSecurityService.setupModule(config, configResult.authWellknownEndpoints);
+        });
+    }
+}
--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using IdentityServer4.Test;
 using System.Collections.Generic;
@@ -13,18 +14,41 @@ namespace IdentityServer
     {
         public static IEnumerable<IdentityResource> Ids =>
             new IdentityResource[]
-            { 
-                new IdentityResources.OpenId()
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email()
             };
 
         public static IEnumerable<ApiResource> Apis =>
-            new ApiResource[] 
-            { };
-        
-        public static IEnumerable<Client> Clients =>
-            new Client[] 
+            new ApiResource[]
             { };
 
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+            new Client
+        {
+            ClientId = "angularClient",
+            ClientName = "JavaScript Client",
+            //ClientUri = "http://identityserver.io",
+            RequireConsent=true,
+
+            AllowedGrantTypes = GrantTypes.Implicit,
+            AllowAccessTokensViaBrowser = true,
+
+            RedirectUris =           { "http://localhost:4200" },
+            PostLogoutRedirectUris = { "http://localhost:4200" },
+            AllowedCorsOrigins =     { "http://localhost:4200" },
+
+            AllowedScopes =
+            {
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile,
+                IdentityServerConstants.StandardScopes.Email,
+            }
+          }
+            };
         public static List<TestUser> Users =>
              new List<TestUser>
             {
